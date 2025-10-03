6a1822d9acd9ba6f6da31ac4952a38ef9c8793c5
uvsq22502717 <volodymyr.grynenko@ens.uvsq.fr>
2025-10-03 14:33:19 +0200

    Numerator denominator with avec les tests

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,8 +1,11 @@
 public class Fraction {
+    int numerator;      // numérateur
+    int denominator;    // dénominateur
 
     @Override
     public String toString() {
         return "Je suis une fraction.";
     }
 }
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Fraction fm = new Fraction();
-        System.out.println(fm); 
+        Fraction f = new Fraction();
+        f.numerator = 3;     
+        f.denominator = 4;    
+
+        // petits tests numerator denominator
+        assert f.numerator == 3;
+        assert f.denominator == 4;
+
+        System.out.println("Tests réussis !");
+        System.out.println(f);
     }
 }
+
 
