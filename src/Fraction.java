38f46b4b5628c174a25df42e069aa54981a323df
uvsq22502717 <volodymyr.grynenko@ens.uvsq.fr>
2025-10-03 14:39:36 +0200

    Constructeurs

--- src/Fraction.java
+++ src/Fraction.java
@@ -2,10 +2,29 @@ public class Fraction {
     int numerator;      // numérateur
     int denominator;    // dénominateur
 
+    // constructeur avec numérateur et dénominateur
+    public Fraction(int n, int d) {
+        numerator = n;
+        denominator = d;
+    }
+
+    // constructeur avec juste le numérateur (dénominateur = 1)
+    public Fraction(int n) {
+        numerator = n;
+        denominator = 1;
+    }
+
+    // constructeur sans argument (0/1)
+    public Fraction() {
+        numerator = 0;
+        denominator = 1;
+    }
+
     @Override
     public String toString() {
-        return "Je suis une fraction.";
+        return numerator + "/" + denominator;
     }
 }
+
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Fraction f = new Fraction();
-        f.numerator = 3;     
-        f.denominator = 4;    
+        // constructeur (numérateur + dénominateur)
+        Fraction f1 = new Fraction(3, 4);
+        assert f1.toString().equals("3/4");
 
-        // petits tests numerator denominator
-        assert f.numerator == 3;
-        assert f.denominator == 4;
+        // constructeur (juste numérateur)
+        Fraction f2 = new Fraction(5);
+        assert f2.toString().equals("5/1");
 
-        System.out.println("Tests réussis !");
-        System.out.println(f);
+        // constructeur sans argument
+        Fraction f3 = new Fraction();
+        assert f3.toString().equals("0/1");
+
+        System.out.println("Tous les tests de constructeurs sont reussi !");
     }
 }
+
 
 
